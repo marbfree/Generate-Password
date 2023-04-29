f23319528f53d8cc32deb7980510a63b6796316f
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-04-29 15:50:47 -0400

    added window prompt code to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,17 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
 
-}
+} 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  const passwordLength = window.prompt("What length would you like your password to be? \nChoose a length between 8 and 128 characters.");
+  const specialChars = window.prompt("Would you like to include special characters?");
+  const upperCase = window.prompt("Would you like to include UPPERCASE letters?");
+  const lowerCase = window.prompt("Would you like to include lowercase letters?");
+}
